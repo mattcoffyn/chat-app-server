58561a386efed49805de2c7517857a48b6ab9ac6
Matt Coffyn <matt.coffyn@gmail.com>
2022-01-17 16:58:07 +0000

    env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
+const url = process.env.CLIENT_URL;
 const io = require('socket.io')({
   cors: {
-    origin: 'http://localhost:3000',
+    origin: url,
     methods: ['GET', 'POST'],
   },
 }).listen(4000);
-
 io.on('connection', (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
